db5d2580979bc53c5645eac7f8397e9c3098a5c6
avigailmo <44937934+avigailmo@users.noreply.github.com>
2023-12-25 13:04:58 +0200

    DATA-5759 Create download_omics_log.sh (#825)

---
+++ python_libs/omics/download_omics_log.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+IFS=$'\n\t'
+
+LOG_GROUP_NAME="/aws/omics/WorkflowLog"
+LOG_STREAM_NAME=$1
+REGION="us-east-1"
+OUTPUT_FILE="${LOG_STREAM_NAME//\//_}.log"
+
+echo ${OUTPUT_FILE}
+result=$(aws logs get-log-events \
+    --start-from-head \
+    --log-group-name=${LOG_GROUP_NAME} \
+    --log-stream-name=${LOG_STREAM_NAME} \
+    --region=${REGION})
+echo ${result} | jq -r .events[].message >> ${OUTPUT_FILE}
+
+nextToken=$(echo $result | jq -r .nextForwardToken)
+while [ -n "$nextToken" ]; do
+    echo ${nextToken}
+    result=$(aws logs get-log-events \
+        --start-from-head \
+        --log-group-name=${LOG_GROUP_NAME} \
+        --log-stream-name=${LOG_STREAM_NAME} \
+        --region=${REGION} \
+        --next-token="${nextToken}")
+
+    if [[ $(echo ${result} | jq -e '.events == []') == "true" ]]; then
+        echo "response with empty events found -> exiting."
+        exit
+    fi
+
+    echo ${result} | jq -r .events[].message >> ${OUTPUT_FILE}
+
+    nextToken=$(echo ${result} | jq -r .nextForwardToken)
+done
