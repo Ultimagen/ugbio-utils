8480972e7fd0fd40d58f7eee3782bfb200b0cb36
gilhornung <gil.hornung@ultimagen.com>
2024-02-05 11:12:40 +0200

    Fixed error in download_omics_log.sh which was due to non-json output format (#927)

--- python_libs/omics/download_omics_log.sh
+++ python_libs/omics/download_omics_log.sh
@@ -10,21 +10,24 @@ OUTPUT_FILE="${LOG_STREAM_NAME//\//_}.log"
 
 echo ${OUTPUT_FILE}
 result=$(aws logs get-log-events \
+    --output json \
     --start-from-head \
     --log-group-name=${LOG_GROUP_NAME} \
     --log-stream-name=${LOG_STREAM_NAME} \
     --region=${REGION})
+
 echo ${result} | jq -r .events[].message >> ${OUTPUT_FILE}
 
 nextToken=$(echo $result | jq -r .nextForwardToken)
 while [ -n "$nextToken" ]; do
     echo ${nextToken}
     result=$(aws logs get-log-events \
-        --start-from-head \
-        --log-group-name=${LOG_GROUP_NAME} \
-        --log-stream-name=${LOG_STREAM_NAME} \
-        --region=${REGION} \
-        --next-token="${nextToken}")
+      --output json \
+      --start-from-head \
+      --log-group-name=${LOG_GROUP_NAME} \
+      --log-stream-name=${LOG_STREAM_NAME} \
+      --region=${REGION} \
+      --next-token="${nextToken}")
 
     if [[ $(echo ${result} | jq -e '.events == []') == "true" ]]; then
         echo "response with empty events found -> exiting."
