be885ac6d1f78f852767cbea6bbebd6e71449d53
avigailmo <44937934+avigailmo@users.noreply.github.com>
2024-06-05 10:01:46 +0300

    get-run-log fixes (#1145)
    output-prefix default '' to prevent adding None prefix when no prefix supplied;
    get run's engine log if run failed and no failed tasks

--- python_libs/omics/get_omics_log.py
+++ python_libs/omics/get_omics_log.py
@@ -6,6 +6,7 @@ import boto3
 from compute_pricing import get_run_info
 
 FAILED_STATUS = "FAILED"
+OMICS_LOG_GROUP = '/aws/omics/WorkflowLog'
 
 
 def get_log_for_task(run_id, task_id=None, session=None, output_path=None, output_prefix='', failed: bool = True):
@@ -32,36 +33,42 @@ def get_log_for_task(run_id, task_id=None, session=None, output_path=None, outpu
         print("------------------------------------------")
         print(f"Getting log for task {task_name} (taskId: {task_id})")
         log_stream_name = f'run/{run_id}/task/{task_id}'
-        log_group_name = '/aws/omics/WorkflowLog'
 
         output = f'{output_prefix}run_{run_id}_task_{task_id}_{task_name}.log'
-        if output_path:
-            os.makedirs(output_path, exist_ok=True)
-            output = f"{output_path}/{output}"
+        fetch_save_log(log_stream_name, output, output_path, session)
 
-        fetch_save_log(log_stream_name, log_group_name, output, session)
+    # in case that the run failed but there're no failed tasks the run's engine log should include the error
+    if run["status"] == FAILED_STATUS and FAILED_STATUS not in [task['status'] for task in run['tasks']]:
+        print(f"Run status is {FAILED_STATUS} but no failed tasks. Getting run's engine log")
+        log_stream_name = f'run/{run_id}/engine'
+        output = f'{output_prefix}run_{run_id}_engine.log'
+        fetch_save_log(log_stream_name, output, output_path, session)
 
 
-def fetch_save_log(log_stream_name, log_group_name, output, session=None):
+def fetch_save_log(log_stream_name, output, output_path, session=None):
     # Get logs client
     if session:
         client = session.client('logs')
     else:
         client = boto3.client('logs')
 
-    print(f"Getting log events for log group '{log_group_name}' and log stream '{log_stream_name}'")
+    print(f"Getting log events for log group '{OMICS_LOG_GROUP}' and log stream '{log_stream_name}'")
 
     # get first page of log events
     response = client.get_log_events(
-        logGroupName=log_group_name,
+        logGroupName=OMICS_LOG_GROUP,
         logStreamName=log_stream_name,
         startFromHead=True
     )
 
     # check if events is not empty
     if not response.get('events'):
-        print(f"No events found for log group '{log_group_name}' and log stream '{log_stream_name}'")
+        print(f"No events found for log group '{OMICS_LOG_GROUP}' and log stream '{log_stream_name}'")
         return
+
+    if output_path:
+        os.makedirs(output_path, exist_ok=True)
+        output = f"{output_path}/{output}"
 
     # write event message to output file
     with open(output, 'w') as file:
@@ -71,7 +78,7 @@ def fetch_save_log(log_stream_name, log_group_name, output, session=None):
         # get next page of log events
         while len(response.get('events')) > 0 and response.get('nextForwardToken'):
             response = client.get_log_events(
-                logGroupName=log_group_name,
+                logGroupName=OMICS_LOG_GROUP,
                 logStreamName=log_stream_name,
                 nextToken=response.get('nextForwardToken'),
                 startFromHead=True
@@ -94,7 +101,7 @@ if __name__ == "__main__":
     parser.add_argument('--no-failed', dest='failed', action='store_false')
     parser.set_defaults(failed=True)
     parser.add_argument('--output', type=str, help="Output dir to save log events", default=None)
-    parser.add_argument('--output-prefix', type=str, help="File name prefix for the output", required=False)
+    parser.add_argument('--output-prefix', type=str, help="File name prefix for the output", required=False, default='')
 
     args = parser.parse_args()
     session = boto3.Session(region_name=args.region)
