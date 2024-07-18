7d640f7a6cb48dcb574e84935e811af560a47d84
Inbal Zelig <inbal.zelig@ultimagen.com>
2024-07-18 12:02:29 +0300

    Add IO to plots (if io data exsits)

--- python_libs/omics/get_preformance_from_omics_log.py
+++ python_libs/omics/get_preformance_from_omics_log.py
@@ -10,17 +10,29 @@ import pandas as pd
 import plotly.graph_objects as go
 from compute_pricing import get_run_cost, get_run_info
 from plotly.subplots import make_subplots
+from enum import Enum
 
+
+class MonitorColumns(Enum):
+    TIME = "time"
+    CPU = "CPU"
+    MEMORY = "Memory"
+    IO_RKB = "IO_rKb/s"
+    IO_WKB = "IO_wKb/s"
+    IOWAIT = "IOWait"
 
 class MonitorLog:
     def __init__(self):
-        self.df = pd.DataFrame(columns=["time","CPU","Memory","IO_rKb/s","IO_wKb/s","IOWait"])
+        self.df_columns = [column.value for column in MonitorColumns]
+        self.df = pd.DataFrame(columns=self.df_columns)
         self.start_time = None
         self.run_time = timedelta(0)
         self.total_cpu = None
         self.total_memory = None
         self.task_name = None
         self.task_id = None
+        # create a dict where key is the column name and value is the color
+        self.colors = dict(zip(self.df_columns, ['black','blue', 'green', 'red', 'purple', 'orange']))
     
     def process_line(self, line):
         if 'MONITORING' not in line:
@@ -224,26 +236,23 @@ def save_figures_to_html(monitor_logs, run_id, output_dir=None, output_prefix=''
     combined_fig = make_subplots(rows=num_rows, cols=1, 
             subplot_titles=[f"Task: {monitor_log.task_name} (id:{monitor_log.task_id})" for monitor_log in plot_tasks])
 
+    add_to_legend = set()
+
     for i, monitor_log in enumerate(plot_tasks, start=1):
-        show_legend = i == 1
-        
-        # Add traces for CPU
-        combined_fig.add_trace(go.Scatter(
-            x=monitor_log.df['time'],
-            y=monitor_log.df['CPU'],
-            mode='lines',
-            name='CPU',
-            line=dict(color='blue'),
-            showlegend=show_legend), row=i, col=1)
-        
-        # Add traces for Memory
-        combined_fig.add_trace(go.Scatter(
-            x=monitor_log.df['time'], 
-            y=monitor_log.df['Memory'], 
-            mode='lines', 
-            name='Memory', 
-            line=dict(color='green'),
-            showlegend=show_legend), row=i, col=1)
+        print(f"Plotting task: {monitor_log.task_name} (id:{monitor_log.task_id})")
+        # Add traces for each metric
+        add_trace(combined_fig, monitor_log, MonitorColumns.CPU.value, i)
+        add_trace(combined_fig, monitor_log, MonitorColumns.MEMORY.value, i)
+        # Add IO metrics only if the coresepong coulmns are not empty and sum is not 0
+        if monitor_log.df[MonitorColumns.IO_RKB.value].notna().any() and monitor_log.df[MonitorColumns.IO_RKB.value].sum() != 0:
+            add_trace(combined_fig, monitor_log, MonitorColumns.IO_RKB.value, i, normelize=True)
+            add_to_legend.add(MonitorColumns.IO_RKB.value)
+        if monitor_log.df[MonitorColumns.IO_WKB.value].notna().any() and monitor_log.df[MonitorColumns.IO_WKB.value].sum() != 0:
+            add_trace(combined_fig, monitor_log, MonitorColumns.IO_WKB.value, i, normelize=True)
+            add_to_legend.add(MonitorColumns.IO_WKB.value)
+        if monitor_log.df[MonitorColumns.IOWAIT.value].notna().any() and monitor_log.df[MonitorColumns.IOWAIT.value].sum() != 0:
+            add_trace(combined_fig, monitor_log, MonitorColumns.IOWAIT.value, i)
+            add_to_legend.add(MonitorColumns.IOWAIT.value)
         
         # y-axis settings
         combined_fig.update_yaxes(title="%", range=[0, 110], row=i, col=1)
@@ -261,20 +270,28 @@ def save_figures_to_html(monitor_logs, run_id, output_dir=None, output_prefix=''
             xanchor="center",
             yanchor="top"
         )
+    
+    # Add missing lables in the legend by adding invisible traces
+    for label in add_to_legend:
+        combined_fig.add_trace(
+            go.Scatter(x=[None], y=[None], mode='lines', name=label),
+            row=1, col=1  # Add it to the first subplot where the leggend appears
+        )
 
     # touch up the layout and add title and subtext
     combined_fig.update_layout(height=300*num_rows+50, title_text=f"Omics {run_id} Performance Plots", title_font=dict(size=20))
-    combined_fig.add_annotation(
-        text=f"Short tasks without a plot: {[f"{monitor_log.task_name} (runtime (H): {monitor_log.run_time.total_seconds()/3600:.2f})" for monitor_log in dont_plot_tasks]}",  # Your subtitle text
-        xref="paper",  # Position relative to the entire plotting area
-        yref="paper",  # Position relative to the entire plotting area
-        x=0,  # Center the text horizontally
-        y=1,  # Adjust this value as needed to position below the title
-        showarrow=False,
-        font=dict(size=12),  # Adjust font size as needed
-        yanchor="bottom",
-        yshift=25  # Shift down by 5 pixels to add more "padding"
-    )
+    if len(dont_plot_tasks) > 0:
+        combined_fig.add_annotation(
+            text=f"Short tasks without a plot: {[f"{monitor_log.task_name} (runtime (H): {monitor_log.run_time.total_seconds()/3600:.2f})" for monitor_log in dont_plot_tasks]}",  # Your subtitle text
+            xref="paper",  # Position relative to the entire plotting area
+            yref="paper",  # Position relative to the entire plotting area
+            x=0,  # Center the text horizontally
+            y=1,  # Adjust this value as needed to position below the title
+            showarrow=False,
+            font=dict(size=12),
+            yanchor="bottom",
+            yshift=25  # Shift down by 25 pixels to add more "padding"
+        )
 
     # Save the combined figure to an HTML file
     combined_fig_output = f"{output_prefix}omics_{run_id}_performance_plots.html"
@@ -282,6 +299,20 @@ def save_figures_to_html(monitor_logs, run_id, output_dir=None, output_prefix=''
         combined_fig_output = f"{output_dir}/{combined_fig_output}"
     print(f"Saving performance plots report to: {combined_fig_output}")
     combined_fig.write_html(combined_fig_output)
+
+def add_trace(combined_fig, monitor_log, col_name, row, normelize=False):
+    show_legend = row == 1
+
+    if normelize:
+        monitor_log.df[col_name] = monitor_log.df[col_name] / monitor_log.df[col_name].max() * 100
+
+    combined_fig.add_trace(go.Scatter(
+            x=monitor_log.df[MonitorColumns.TIME.value],
+            y=monitor_log.df[col_name],
+            mode='lines',
+            name=col_name,
+            line=dict(color=monitor_log.colors[col_name]),
+            showlegend=show_legend), row=row, col=1)
 
 if __name__ == "__main__":
     parser = ArgumentParser()
