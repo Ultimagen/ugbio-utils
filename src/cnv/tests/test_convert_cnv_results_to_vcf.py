952a89a87dbeb7637ae6e7ff32cd45d10df3a266
avigailmo <44937934+avigailmo@users.noreply.github.com>
2024-08-01 15:25:47 +0300

    update CONFIDENCE values; unzip then check files; align with ugvc dependencies (#6)
    * update CONFIDENCE values
    
    * install tabix
    
    * unzip

--- src/cnv/tests/test_convert_cnv_results_to_vcf.py
+++ src/cnv/tests/test_convert_cnv_results_to_vcf.py
@@ -1,6 +1,8 @@
 from os.path import join as pjoin
-import hashlib
 import warnings
+import gzip
+import shutil
+import filecmp
 
 warnings.filterwarnings('ignore')
 
@@ -9,13 +11,18 @@ from . import get_resource_dir
 from ugbio_cnv import convert_cnv_results_to_vcf
 
 
+def unzip_file(zipped_file_name):
+    with gzip.open(zipped_file_name, 'rb') as f_in:
+        out_file_name = zipped_file_name.rstrip('.gz')
+        with open(out_file_name, 'wb') as f_out:
+            shutil.copyfileobj(f_in, f_out)
+    return out_file_name
+
+
 def compare_zipped_files(a, b):
-    fileA = hashlib.sha256(open(a, 'rb').read()).digest()
-    fileB = hashlib.sha256(open(b, 'rb').read()).digest()
-    if fileA == fileB:
-        return True
-    else:
-        return False
+    a_unzipped = unzip_file(a)
+    b_unzipped = unzip_file(b)
+    assert filecmp.cmp(a_unzipped, b_unzipped)
 
 
 class TestConvertCnvResultsToVcf:
@@ -41,4 +48,4 @@ class TestConvertCnvResultsToVcf:
         ])
 
         out_vcf_file = pjoin(tmpdir, sample_name + '.cnv.vcf.gz')
-        assert compare_zipped_files(out_vcf_file, expected_out_vcf)
+        compare_zipped_files(out_vcf_file, expected_out_vcf)
