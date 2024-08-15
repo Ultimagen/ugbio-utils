2a311104c06096186415d7dde089c5569a303333
Gavrie Philipson <gavrie@philipson.co.il>
2024-08-15 22:08:20 +0300

    Separate read and write stages

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use std::io::Write;
 
 use noodles::bed;
 use noodles::bed::record::OptionalFields;
+use noodles::core::Position;
 
 /*
   bedtools groupby -c 3 -o count < all.bed > grouped.bed
@@ -28,17 +29,22 @@ fn read_records(input: impl Read) {
 }
 
 fn write_records(output: impl Write) {
-    while false {
-        let buf_output = io::BufWriter::new(output);
-        let mut bed_output = bed::Writer::new(buf_output);
+    let buf_output = io::BufWriter::new(output);
+    let mut bed_output = bed::Writer::new(buf_output);
 
+    /* loop... */
+    {
         let count = 42;
         let optional_fields = OptionalFields::from(vec![count.to_string()]);
 
+        let name = "chr1";
+        let start_position = Position::try_from(10107).unwrap();
+        let end_position = Position::try_from(10108).unwrap();
+
         let record_out = bed::Record::<3>::builder()
-            .set_reference_sequence_name(record_in.reference_sequence_name())
-            .set_start_position(record_in.start_position())
-            .set_end_position(record_in.end_position())
+            .set_reference_sequence_name(name)
+            .set_start_position(start_position)
+            .set_end_position(end_position)
             .set_optional_fields(optional_fields)
             .build()
             .unwrap();
