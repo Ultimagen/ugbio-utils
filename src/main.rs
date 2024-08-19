a10fc78c05005a018bd90105d37ee587cc2b6814
Gavrie Philipson <gavrie@philipson.co.il>
2024-08-19 15:38:58 +0300

    Read vcf.gz into SQLite

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,4 @@
-use std::fs::File;
 use std::io;
-use std::io::BufReader;
 use std::io::Read;
 use std::io::Write;
 use std::time::Instant;
@@ -10,86 +8,77 @@ use anyhow::Result;
 use noodles::bed;
 use noodles::core::Position;
 use noodles::vcf;
+use noodles::vcf::variant::record::Ids; // trait
 
+use rusqlite::named_params;
 use rusqlite::Connection;
 
-/*
-fn vcf_read() -> Result<()> {
-    // Open the database connection
-    let conn = Connection::open("vcf.db")?;
+struct Variants {
+    conn: Connection,
+    header: vcf::Header,
+}
 
-    // Create the table if it doesn't exist
-    conn.execute(
-        "CREATE TABLE IF NOT EXISTS variants (
+impl Variants {
+    fn from_vcf(input: impl Read) -> Result<Self> {
+        let mut conn = Connection::open("vcf.db")?;
+
+        conn.execute(
+            "CREATE TABLE variants (
             chrom TEXT,
             pos INTEGER,
             id TEXT,
             ref TEXT,
             alt TEXT,
             qual REAL,
-            filter TEXT
+            filter TEXT,
+            info TEXT
         )",
-        [],
-    )?;
+            [],
+        )?;
 
-    // Open the VCF file
-    let file = File::open("path/to/your/file.vcf.gz").expect("Failed to open file");
-    let reader = BufReader::new(file);
-    let mut vcf_reader = vcf::record::Builder::default()
-        .build_bgzf(reader)
-        .expect("Failed to build VCF reader");
+        // let reader = Builder::default().build_from_path("sample.vcf")?;
+        let mut reader = vcf::io::reader::Builder::default()
+            .set_compression_method(vcf::io::CompressionMethod::Bgzf)
+            .build_from_reader(input)?;
 
-    // Read the VCF header
-    let header = vcf_reader.read_header().expect("Failed to read VCF header");
+        let header = reader.read_header()?;
+        // eprintln!("Header: {header:#?}");
 
-    // Prepare the SQL statement
-    let mut stmt = conn.prepare(
-        "INSERT INTO variants (chrom, pos, id, ref, alt, qual, filter)
-         VALUES (?, ?, ?, ?, ?, ?, ?)",
-    )?;
+        let before = Instant::now();
 
-    // Iterate over records in the VCF file
-    for result in vcf_reader.records(&header) {
-        let record = result.expect("Failed to read VCF record");
+        let tx = conn.transaction()?;
+        {
+            let mut stmt = tx.prepare(
+                "INSERT INTO variants
+            (chrom, pos, id, ref, alt, qual, filter, info)
+         VALUES
+            (:chrom, :pos, :id, :ref, :alt, :qual, :filter, :info)",
+            )?;
 
-        let chrom = record.chromosome().to_string();
-        let pos = record.position().get() as i64;
-        let id = record.ids().join(",");
-        let ref_allele = record.reference_bases().to_string();
-        let alt_alleles = record
-            .alternate_bases()
-            .iter()
-            .map(|allele| allele.to_string())
-            .collect::<Vec<String>>()
-            .join(",");
-        let qual = record.quality_score().map(|q| q.get()).unwrap_or(0.0);
-        let filter = record
-            .filters(&header)
-            .map(|filters| {
-                filters
-                    .iter()
-                    .map(|f| f.to_string())
-                    .collect::<Vec<String>>()
-                    .join(";")
-            })
-            .unwrap_or_else(|| "PASS".to_string());
+            for result in reader.records() {
+                let record = result?;
 
-        // Insert the record into the database
-        stmt.execute([
-            &chrom,
-            &pos.to_string(),
-            &id,
-            &ref_allele,
-            &alt_alleles,
-            &qual.to_string(),
-            &filter,
-        ])?;
+                // eprintln!("Records: {record:#?}");
+
+                stmt.execute(named_params! {
+                    ":chrom": record.reference_sequence_name(),
+                    ":pos": record.variant_start().expect("pos")?.get(),
+                    ":id": record.ids().iter().next(), // Take only first ID (if any)
+                    ":ref": record.reference_bases(),
+                    ":alt": record.alternate_bases().as_ref(),
+                    ":qual": record.quality_score().transpose()?,
+                    ":filter": record.filters().as_ref(),
+                    ":info": record.info().as_ref(),
+                })?;
+            }
+        }
+        tx.commit()?;
+
+        eprintln!("vcf_read: {:.2?}", before.elapsed());
+
+        Ok(Self { conn, header })
     }
-
-    println!("VCF data has been successfully imported into the database.");
-    Ok(())
 }
-*/
 
 /*
     TODO: Use the vcf.gz format directly instead of converting to bed and then filtering:
@@ -117,7 +106,7 @@ fn read_records(input: impl Read, conn: &mut Connection) -> Result<()> {
     let mut bed_input = bed::Reader::new(buf_input);
 
     conn.execute(
-        "CREATE TABLE IF NOT EXISTS records (
+        "CREATE TABLE records (
            name TEXT NOT NULL,
            start INTEGER NOT NULL,
            end INTEGER NOT NULL
@@ -186,5 +175,7 @@ fn write_records(output: impl Write, conn: &mut Connection) -> Result<()> {
 }
 
 fn main() -> Result<()> {
-    groupby(io::stdin(), io::stdout())
+    // groupby(io::stdin(), io::stdout())
+    let _variants = Variants::from_vcf(io::stdin())?;
+    Ok(())
 }
