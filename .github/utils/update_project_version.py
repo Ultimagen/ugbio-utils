329d9f9307579bc30f6bd556e5f78d2448bb705f
avigailmo <avigail.moldovan@ultimagen.com>
2024-09-18 09:32:01 +0300

    update version to update Cargo files' version

--- .github/utils/update_project_version.py
+++ .github/utils/update_project_version.py
@@ -9,12 +9,10 @@ from re import sub
 version_pattern = re.compile(r'^(version\s*=\s*")[^"]*(".*)$', re.MULTILINE)
 
 
-def main(version):
-    version = version.lstrip('v')
-    # Find all project.toml files
-    project_files = glob.glob('**/pyproject.toml', recursive=True)
+def _update_toml_version(glob_regex, project_key_name):
+    toml_files = glob.glob('**/pyproject.toml', recursive=True)
 
-    for file_path in project_files:
+    for file_path in pyproject_files:
         print(f"updating '{file_path}' version to {version}")
         with open(file_path, 'rb') as file:
             data = tomllib.load(file)
@@ -24,8 +22,17 @@ def main(version):
             tomli_w.dump(data, file)
 
 
+def main(version):
+    version = version.lstrip('v')
+    # Find and update all pyproject.toml files
+    _update_toml_version("**/pyproject.toml", "project")
+    # Find and update all Cargo.toml files
+    _update_toml_version("**/Cargo.toml", "package")
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("version", help="Version to publish modules with", type=str)
+    parser.add_argument(
+        "version", help="Version to publish modules with", type=str)
     args = parser.parse_args()
     main(args.version)
