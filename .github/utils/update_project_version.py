886d6908b74eb85411a0a8af5ddef9d82493d265
avigailmo <44937934+avigailmo@users.noreply.github.com>
2024-09-26 15:07:06 +0300

    make workspaces list dynamic; manage Rust modules (#30)
    * make workspaces list dynamic
    
    * update version to update Cargo files' version
    
    * alias to run tests
    
    * update README
    
    * add run_tests script
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- .github/utils/update_project_version.py
+++ .github/utils/update_project_version.py
@@ -5,27 +5,32 @@ import glob
 import argparse
 from re import sub
 
-# Define the version pattern
-version_pattern = re.compile(r'^(version\s*=\s*")[^"]*(".*)$', re.MULTILINE)
 
 
-def main(version):
-    version = version.lstrip('v')
-    # Find all project.toml files
-    project_files = glob.glob('**/pyproject.toml', recursive=True)
+def _update_toml_version(glob_regex, project_key_name, version):
+    toml_files = glob.glob(glob_regex, recursive=True)
 
-    for file_path in project_files:
+    for file_path in toml_files:
         print(f"updating '{file_path}' version to {version}")
         with open(file_path, 'rb') as file:
             data = tomllib.load(file)
             # Replace the version string
-        data['project']['version'] = version
+        data[project_key_name]['version'] = version
         with open(file_path, 'wb') as file:
             tomli_w.dump(data, file)
 
 
+def main(version):
+    version = version.lstrip('v')
+    # Find and update all pyproject.toml files
+    _update_toml_version("**/pyproject.toml", "project", version)
+    # Find and update all Cargo.toml files
+    _update_toml_version("**/Cargo.toml", "package", version)
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("version", help="Version to publish modules with", type=str)
+    parser.add_argument(
+        "version", help="Version to publish modules with", type=str)
     args = parser.parse_args()
     main(args.version)
