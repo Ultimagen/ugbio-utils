42d6b279cec28f4e7cb39ba2860518414e062dee
avigailmo <avigail.moldovan@ultimagen.com>
2024-10-08 10:05:16 +0300

    review changes

--- src/core/ugbio_core/logger.py
+++ src/core/ugbio_core/logger.py
@@ -8,7 +8,7 @@ logger.setLevel(logging.INFO)
 # create formatter
 formatter = logging.Formatter("%(asctime)s - %(module)s - %(levelname)s - %(message)s")
 
-# create console handler and set level to debug
+# create console handler and set level to info
 ch = logging.StreamHandler(stream=sys.stderr)
 ch.setLevel(logging.INFO)
 ch.setFormatter(formatter)
