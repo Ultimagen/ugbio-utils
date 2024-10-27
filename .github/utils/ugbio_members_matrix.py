b64e26834f4f585d56942ba099fcaebf2caf180c
Inbal Zelig <inbal.zelig@ultimagen.com>
2024-10-27 10:56:02 +0200

    fix pre-commit errors

--- .github/utils/ugbio_members_matrix.py
+++ .github/utils/ugbio_members_matrix.py
@@ -1,21 +1,20 @@
 #!/usr/bin/env python3
-import os
-import json
 import argparse
+import json
+import os
 
 INTERNAL_MEMBERS = ["core", "omics"]
 
-def build_members_matrix(src_folder: str, keep_internal: bool):
+
+def build_members_matrix(src_folder: str, *, keep_internal: bool):
     # List directories in the src_folder that don't contain "__"
-    directories = [
-        d for d in os.listdir(src_folder)
-        if os.path.isdir(os.path.join(src_folder, d)) and "__" not in d
-    ]
+    directories = [d for d in os.listdir(src_folder) if os.path.isdir(os.path.join(src_folder, d)) and "__" not in d]
 
     if not keep_internal:
         directories = [d for d in directories if d not in INTERNAL_MEMBERS]
 
     print(json.dumps(directories, indent=4, sort_keys=True))
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -24,4 +23,4 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    build_members_matrix(args.src_folder, args.keep_internal)
+    build_members_matrix(args.src_folder, keep_internal=args.keep_internal)
