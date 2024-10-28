d22379f5f0b00d49712111960fc7b19cf542a161
Inbal Zelig <inbal.zelig@ultimagen.com>
2024-10-28 15:53:46 +0200

    tests for get_omics_log
    fix get omics logs

--- src/omics/tests/unit/test_get_omics_log.py
+++ src/omics/tests/unit/test_get_omics_log.py
@@ -1,2 +1,111 @@
-def test_get_log_for_task():
-    pass  # TODO: add tests
+from unittest.mock import Mock, patch
+
+import pytest
+from ugbio_omics.get_omics_log import OMICS_LOG_GROUP, fetch_save_log, get_log_for_task
+
+
+@pytest.fixture
+def mock_boto3_client():
+    with patch("boto3.client") as mock_client:
+        # Create separate mock clients for "logs" and "omics"
+        mock_logs_client = Mock()
+        mock_omics_client = Mock()
+
+        # Configure the side_effect to return the appropriate mock client based on the service name
+        def client_side_effect(service_name, *args, **kwargs):
+            if service_name == "logs":
+                return mock_logs_client
+            elif service_name == "omics":
+                return mock_omics_client
+            else:
+                raise ValueError(f"Unexpected service name: {service_name}")
+
+        mock_boto3_client.side_effect = client_side_effect
+
+        # Configure the mock clients
+        mock_logs_client.get_log_events.return_value = {
+            "events": [{"message": "log message 1"}, {"message": "log message 2"}]
+        }
+        yield mock_client
+
+
+@pytest.fixture
+def mock_get_run_info():
+    with patch("ugbio_omics.get_omics_log.get_run_info") as mock_get_run_info:
+        mock_get_run_info.return_value = {
+            "tasks": [
+                {"taskId": "taskID1", "name": "taskName1", "status": "SUCCEEDED"},
+                {"taskId": "taskID2", "name": "taskName2", "status": "FAILED"},
+            ],
+            "status": "SUCCEEDED",
+        }
+        yield mock_get_run_info
+
+
+def test_get_log_for_task_all_tasks(mock_boto3_client, mock_get_run_info, tmpdir):
+    get_log_for_task("runID", output_path=tmpdir, output_prefix="test_", failed=False)
+
+    mock_get_run_info.assert_called_once()
+    mock_boto3_client.assert_called_with("logs")
+    assert len(tmpdir.listdir()) == 2  # check that 2 log files created for all tasks
+    assert any(f.basename.endswith("taskID1_taskName1.log") for f in tmpdir.listdir())
+    assert any(f.basename.endswith("taskID2_taskName2.log") for f in tmpdir.listdir())
+
+
+def test_get_log_for_task_failed_tasks(mock_boto3_client, mock_get_run_info, tmpdir):
+    get_log_for_task("runID", output_path=tmpdir, output_prefix="test_")
+
+    mock_get_run_info.assert_called_once()
+    mock_boto3_client.assert_called_with("logs")
+    assert len(tmpdir.listdir()) == 1  # check that one log file created for the failed task
+    assert any(f.basename.endswith("taskID2_taskName2.log") for f in tmpdir.listdir())
+
+
+def test_get_log_for_task_get_engine_log(mock_boto3_client, tmpdir):
+    with patch("ugbio_omics.get_omics_log.get_run_info") as mock_get_run_info:
+        mock_get_run_info.return_value = {
+            "tasks": [
+                {"taskId": "taskID1", "name": "taskName1", "status": "SUCCEEDED"},
+                {"taskId": "taskID2", "name": "taskName2", "status": "SUCCEEDED"},
+            ],
+            "status": "FAILED",
+        }
+
+        get_log_for_task("runID", output_path=tmpdir, output_prefix="test_")
+
+        mock_get_run_info.assert_called_once()
+    mock_boto3_client.assert_called_with("logs")
+    assert len(tmpdir.listdir()) == 1  # check that engine log created
+    assert any(f.basename.endswith("engine.log") for f in tmpdir.listdir())
+
+
+@patch("ugbio_omics.get_omics_log.boto3")
+def test_fetch_save_log_with_events(mock_boto3, tmpdir):
+    mock_boto3.client.return_value.get_log_events.side_effect = [
+        {"events": [{"message": "log message 1"}, {"message": "log message 2"}], "nextForwardToken": "token"},
+        {"events": [{"message": "log message 3"}], "nextForwardToken": None},
+    ]
+
+    fetch_save_log(log_stream_name="log_stream", output="output.log", output_path=tmpdir)
+
+    mock_boto3.client.assert_called_with("logs")
+    mock_boto3.client.return_value.get_log_events.assert_any_call(
+        logGroupName=OMICS_LOG_GROUP, logStreamName="log_stream", nextToken="token", startFromHead=True
+    )
+    mock_boto3.client.return_value.get_log_events.assert_any_call(
+        logGroupName=OMICS_LOG_GROUP, logStreamName="log_stream", startFromHead=True
+    )
+    assert len(tmpdir.listdir()) == 1
+
+
+@patch("ugbio_omics.get_omics_log.boto3")
+def test_fetch_save_log_no_events(mock_boto3, tmpdir):
+    mock_boto3.client.return_value.get_log_events.return_value = {"events": [], "nextForwardToken": None}
+
+    fetch_save_log(log_stream_name="log_stream", output="output.log", output_path=tmpdir)
+
+    mock_boto3.client.assert_called_with("logs")
+    mock_boto3.client.return_value.get_log_events.assert_called_once_with(
+        logGroupName=OMICS_LOG_GROUP, logStreamName="log_stream", startFromHead=True
+    )
+    assert not tmpdir.listdir()
