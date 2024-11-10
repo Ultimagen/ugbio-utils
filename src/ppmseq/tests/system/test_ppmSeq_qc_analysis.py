a5aeefc5913e5d537d5e95ce02e383e69a022c3f
Itai Rusinek <itai.rusinek@ultimagen.com>
2024-11-10 11:59:39 +0000

    Attempted untested fix in QC report - generate flow signal, append a C to the end of the sequence for the case where that C was trimmed
    Added test data from the relevant run

--- src/ppmseq/tests/system/test_ppmSeq_qc_analysis.py
+++ src/ppmseq/tests/system/test_ppmSeq_qc_analysis.py
@@ -78,3 +78,37 @@ def test_ppmseq_analysis_ppmseq_v1(tmpdir, resources_dir):
             "--legacy-histogram-column-names",
         ]
     )
+
+
+def test_ppmseq_analysis_ppmseq_post_native_adapter_trimming(tmpdir, resources_dir):
+    this_resource_dir = pjoin(resources_dir, "409271-UGAv3-377_post_native_adapter_trimming")
+    trimmer_histogram = pjoin(
+        this_resource_dir,
+        "Start_loop_name.Start_loop_pattern_fw.End_loop_name.End_loop_pattern_fw.Stem_end_length.histogram.csv",
+    )
+    trimmer_failure_codes = pjoin(
+        this_resource_dir,
+        "409271-UGAv3-377-CAGAATACATGCGAT_CR0-244.failure_codes.csv",
+    )
+    sorter_csv = pjoin(this_resource_dir, "409271-UGAv3-377-CAGAATACATGCGAT_CR0-244.csv")
+    sorter_json = pjoin(this_resource_dir, "409271-UGAv3-377-CAGAATACATGCGAT_CR0-244.json")
+
+    ppmSeq_qc_analysis.run(
+        [
+            "ppmSeq_qc_analysis",
+            "--adapter-version",
+            "v1",
+            "--trimmer-histogram-csv",
+            trimmer_histogram,
+            "--trimmer-failure-codes-csv",
+            trimmer_failure_codes,
+            "--sorter-stats-csv",
+            sorter_csv,
+            "--sorter-stats-json",
+            sorter_json,
+            "--output-path",
+            tmpdir.dirname,
+            "--output-basename",
+            "409271-UGAv3-377-CAGAATACATGCGAT_CR0-244",
+        ]
+    )
