387c20e57b4a8bae55da084dec165d4adf1fb117
Ubuntu <ubuntu@ip-172-31-108-234.ec2.internal>
2024-11-25 07:11:11 +0000

    work2

--- onlink_test.py
+++ onlink_test.py
@@ -12,7 +12,7 @@ import multiprocessing as mp
 
 
 
-def process_cram_file(cram_file: str, output_path: str):
+def process_cram_file(cram_file: str, output_path: str, output_name: str="output"):
     """
     Processes a CRAM file and saves the output to the specified path.
 
@@ -45,10 +45,8 @@ def process_cram_file(cram_file: str, output_path: str):
     map_identifiers = np.unique(mappings)
 
     matrix = confusion_matrix_parallel(merged_segments, mappings, map_identifiers, combined_sequences,indels=1)
-
-    print(matrix)
-    
-    output_path = os.path.join(output_path, "output.npy")
+       
+    output_path = os.path.join(output_path, f"{output_name}.npy")
 
     np.save(output_path, matrix)
 
@@ -397,14 +395,15 @@ def main():
     parser = argparse.ArgumentParser(description="Process a CRAM file and save the output.")
     parser.add_argument("cram_file", type=str, help="Path to the input CRAM file.")
     parser.add_argument("output_path", type=str, help="Path to save the processed output.")
+    parser.add_argument("--output_name", type=str, default="output", help="Name of the output file.")
     
     args = parser.parse_args()
     
     # Call the processing function
-    process_cram_file(args.cram_file, args.output_path)
+    process_cram_file(args.cram_file, args.output_path, args.output_name)
 
 if __name__ == "__main__":
     main()
 
 
-#   $ python ~/ugbio-utils/onlink_test.py /data/Runs/olink/409959-Olink_HT31-Z0008.1M.bam /data/Runs/olink/matrix_output.bin
+#   $ python ~/ugbio-utils/onlink_test.py /data/Runs/olink/409959-Olink_HT31-Z0008.1M.bam /data/Runs/olink/ --output_name mat1
