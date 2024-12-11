a9e2a1c299886aa5c95efc7b9c7066091e1f58ab
avigailmo <avigail.moldovan@ultimagen.com>
2024-12-11 23:03:33 +0200

    creat on fly

--- src/freec/tests/unit/test_bigwig_to_bedgraph.py
+++ src/freec/tests/unit/test_bigwig_to_bedgraph.py
@@ -1,6 +1,8 @@
 import filecmp
+from os.path import join as pjoin
 from pathlib import Path
 
+import pyBigWig as pBW
 import pytest
 from ugbio_freec.bigwig_to_bedgraph import bigwig_to_bedgraph
 
@@ -10,10 +12,22 @@ def resources_dir():
     return Path(__file__).parent.parent / "resources"
 
 
+def create_temp_bw(tmpdir, name):
+    with pBW.open(pjoin(tmpdir, name), "w") as bw:
+        bw.addHeader([("chr1", 1000)])
+        bw.addEntries(
+            ["chr1", "chr1", "chr1"],  # Chromosomes
+            [0, 200, 500],  # Start positions
+            ends=[100, 300, 600],  # End positions
+            values=[1.0, 2.5, -3.0],  # Values
+        )
+    return pjoin(tmpdir, name)
+
+
 def test_bigwig_to_bedgraph(tmpdir, resources_dir):
-    bigwig_file = resources_dir / "test_input.bigWig"
-    bedgraph_expected_file = resources_dir / "texpected_output.bedGraph"
+    tmp_bw_file = create_temp_bw(tmpdir, "test1.bw")
+    bedgraph_expected_file = resources_dir / "expected_output.bedGraph"
     bedgraph_actual_file = tmpdir / "output.bedGraph"
-    bigwig_to_bedgraph(bigwig_file, bedgraph_actual_file)
+    bigwig_to_bedgraph(tmp_bw_file, bedgraph_actual_file)
 
     assert filecmp.cmp(bedgraph_expected_file, bedgraph_actual_file)
