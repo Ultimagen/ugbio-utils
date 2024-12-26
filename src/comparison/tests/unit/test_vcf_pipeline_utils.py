af71aaa244c59666a73f4215294468ffb1577535
avigailmo <avigail.moldovan@ultimagen.com>
2024-12-26 14:41:41 +0200

    test fixes

--- src/comparison/tests/unit/test_vcf_pipeline_utils.py
+++ src/comparison/tests/unit/test_vcf_pipeline_utils.py
@@ -3,29 +3,31 @@ import subprocess
 from collections import Counter
 from os.path import basename, exists
 from os.path import join as pjoin
+from pathlib import Path
 
 import pandas as pd
 import pysam
+import pytest
 from simppl.simple_pipeline import SimplePipeline
-from ugbio_core.h5_utils import read_hdf
-from ugvc.comparison.vcf_pipeline_utils import (
+from ugbio_comparison.vcf_pipeline_utils import (
     VcfPipelineUtils,
     _fix_errors,
     bed_file_length,
     close_to_hmer_run,
     vcf2concordance,
 )
-from ugvc.vcfbed import vcftools
-from ugvc.vcfbed.interval_file import IntervalFile
-
-from test import get_resource_dir, test_dir
-
-inputs_dir = get_resource_dir(__file__)
-common_dir = pjoin(test_dir, "resources", "general")
+from ugbio_core.h5_utils import read_hdf
+from ugbio_core.vcfbed import vcftools
+from ugbio_core.vcfbed.interval_file import IntervalFile
 
 
-def test_fix_errors():
-    data = read_hdf(pjoin(inputs_dir, "h5_file_unitest.h5"), key="concordance")
+@pytest.fixture
+def resources_dir():
+    return Path(__file__).parent.parent / "resources"
+
+
+def test_fix_errors(resources_dir):
+    data = read_hdf(pjoin(resources_dir, "h5_file_unitest.h5"), key="concordance")
     fixed_df = _fix_errors(data)
     assert all(
         fixed_df[((fixed_df["call"] == "TP") & ((fixed_df["base"] == "TP") | (fixed_df["base"].isna())))][
@@ -54,8 +56,8 @@ def test_fix_errors():
     )
 
 
-def test_transform_hom_calls_to_het_calls(tmpdir):
-    input_vcf = pjoin(inputs_dir, "dv.input.vcf.gz")
+def test_transform_hom_calls_to_het_calls(tmpdir, resources_dir):
+    input_vcf = pjoin(resources_dir, "dv.input.vcf.gz")
     vpu = VcfPipelineUtils()
     shutil.copyfile(input_vcf, pjoin(tmpdir, basename(input_vcf)))
     expected_output_file = pjoin(tmpdir, basename(input_vcf).replace(".vcf.gz", ".rev.hom.ref.vcf.gz"))
@@ -73,55 +75,54 @@ def test_transform_hom_calls_to_het_calls(tmpdir):
 
 
 class TestVCF2Concordance:
-    def test_qual_not_nan(self):
-        input_vcf = pjoin(inputs_dir, "chr2.vcf.gz")
-        concordance_vcf = pjoin(inputs_dir, "chr2.conc.vcf.gz")
+    def test_qual_not_nan(self, resources_dir):
+        input_vcf = pjoin(resources_dir, "chr2.vcf.gz")
+        concordance_vcf = pjoin(resources_dir, "chr2.conc.vcf.gz")
         result = vcf2concordance(input_vcf, concordance_vcf)
         assert pd.isna(result.query("classify!='fn'").qual).sum() == 0
         assert pd.isna(result.query("classify!='fn'").sor).sum() == 0
 
-    def test_filtered_out_missing(self):
-        input_vcf = pjoin(inputs_dir, "hg002.vcf.gz")
-        concordance_vcf = pjoin(inputs_dir, "hg002.conc.vcf.gz")
+    def test_filtered_out_missing(self, resources_dir):
+        input_vcf = pjoin(resources_dir, "hg002.vcf.gz")
+        concordance_vcf = pjoin(resources_dir, "hg002.conc.vcf.gz")
         result = vcf2concordance(input_vcf, concordance_vcf)
         assert ((result["call"] == "IGN") & (pd.isna(result["base"]))).sum() == 0
 
-    def test_filtered_out_tp_became_fn(self):
-        input_vcf = pjoin(inputs_dir, "hg002.vcf.gz")
-        concordance_vcf = pjoin(inputs_dir, "hg002.conc.vcf.gz")
+    def test_filtered_out_tp_became_fn(self, resources_dir):
+        input_vcf = pjoin(resources_dir, "hg002.vcf.gz")
+        concordance_vcf = pjoin(resources_dir, "hg002.conc.vcf.gz")
         result = vcf2concordance(input_vcf, concordance_vcf)
         take = result[(result["call"] == "IGN") & (result["base"] == "FN")]
         assert take.shape[0] > 0
         assert (take["classify"] == "fn").all()
 
-    def test_excluded_regions_are_ignored(self):
-        input_vcf = pjoin(inputs_dir, "hg002.excluded.vcf.gz")
-        concordance_vcf = pjoin(inputs_dir, "hg002.excluded.conc.vcf.gz")
+    def test_excluded_regions_are_ignored(self, resources_dir):
+        input_vcf = pjoin(resources_dir, "hg002.excluded.vcf.gz")
+        concordance_vcf = pjoin(resources_dir, "hg002.excluded.conc.vcf.gz")
         result = vcf2concordance(input_vcf, concordance_vcf)
         assert (result["call"] == "OUT").sum() == 0
         assert (result["base"] == "OUT").sum() == 0
 
-    def test_all_ref_never_false_negative(self):
-        input_vcf = pjoin(inputs_dir, "hg002.allref.vcf.gz")
-        concordance_vcf = pjoin(inputs_dir, "hg002.allref.conc.vcf.gz")
+    def test_all_ref_never_false_negative(self, resources_dir):
+        input_vcf = pjoin(resources_dir, "hg002.allref.vcf.gz")
+        concordance_vcf = pjoin(resources_dir, "hg002.allref.conc.vcf.gz")
         result = vcf2concordance(input_vcf, concordance_vcf)
         calls = result[result["gt_ground_truth"] == (0, 0)].classify_gt.value_counts()
         assert "fn" not in calls.index
 
 
 class TestVCFEvalRun:
-    ref_genome = pjoin(common_dir, "sample.fasta")
-    sample_calls = pjoin(inputs_dir, "sample.sd.vcf.gz")
-    truth_calls = pjoin(inputs_dir, "gtr.sample.sd.vcf.gz")
-
-    def test_vcfeval_run_ignore_filter(self, tmp_path):
+    def test_vcfeval_run_ignore_filter(self, tmp_path, resources_dir):
+        ref_genome = pjoin(resources_dir, "sample.fasta")
+        sample_calls = pjoin(resources_dir, "sample.sd.vcf.gz")
+        truth_calls = pjoin(resources_dir, "gtr.sample.sd.vcf.gz")
         sp = SimplePipeline(0, 100)
-        high_conf = IntervalFile(None, pjoin(inputs_dir, "highconf.interval_list"))
+        high_conf = IntervalFile(None, pjoin(resources_dir, "highconf.interval_list"))
         VcfPipelineUtils(sp).run_vcfeval_concordance(
-            input_file=self.sample_calls,
-            truth_file=self.truth_calls,
+            input_file=sample_calls,
+            truth_file=truth_calls,
             output_prefix=str(tmp_path / "sample.ignore_filter"),
-            ref_genome=self.ref_genome,
+            ref_genome=ref_genome,
             evaluation_regions=str(high_conf.as_bed_file()),
             comparison_intervals=str(high_conf.as_bed_file()),
             input_sample="sm1",
@@ -135,14 +136,17 @@ class TestVCFEvalRun:
             calls = Counter([x.info["CALL"] for x in vcf])
         assert calls == {"FP": 99, "TP": 1}
 
-    def test_vcfeval_run_use_filter(self, tmp_path):
+    def test_vcfeval_run_use_filter(self, tmp_path, resources_dir):
+        ref_genome = pjoin(resources_dir, "sample.fasta")
+        sample_calls = pjoin(resources_dir, "sample.sd.vcf.gz")
+        truth_calls = pjoin(resources_dir, "gtr.sample.sd.vcf.gz")
         sp = SimplePipeline(0, 100)
-        high_conf = IntervalFile(None, pjoin(inputs_dir, "highconf.interval_list"))
+        high_conf = IntervalFile(None, pjoin(resources_dir, "highconf.interval_list"))
         VcfPipelineUtils(sp).run_vcfeval_concordance(
-            input_file=self.sample_calls,
-            truth_file=self.truth_calls,
+            input_file=sample_calls,
+            truth_file=truth_calls,
             output_prefix=str(tmp_path / "sample.use_filter"),
-            ref_genome=self.ref_genome,
+            ref_genome=ref_genome,
             evaluation_regions=str(high_conf.as_bed_file()),
             comparison_intervals=str(high_conf.as_bed_file()),
             input_sample="sm1",
@@ -158,9 +162,9 @@ class TestVCFEvalRun:
         assert calls == {"FP": 91, "TP": 1, "IGN": 8}
 
 
-def test_intersect_bed_files(mocker, tmp_path):
-    bed1 = pjoin(inputs_dir, "bed1.bed")
-    bed2 = pjoin(inputs_dir, "bed2.bed")
+def test_intersect_bed_files(mocker, tmp_path, resources_dir):
+    bed1 = pjoin(resources_dir, "bed1.bed")
+    bed2 = pjoin(resources_dir, "bed2.bed")
     output_path = pjoin(tmp_path, "output.bed")
 
     # Test with simple pipeline
@@ -177,14 +181,14 @@ def test_intersect_bed_files(mocker, tmp_path):
     assert exists(output_path)
 
 
-def test_bed_file_length():
-    bed1 = pjoin(inputs_dir, "bed1.bed")
+def test_bed_file_length(resources_dir):
+    bed1 = pjoin(resources_dir, "bed1.bed")
     result = bed_file_length(bed1)
     assert result == 3026
 
 
-def test_close_to_hmer_run():
-    input_vcf = vcftools.get_vcf_df(pjoin(inputs_dir, "hg19.vcf.gz"))
-    runs_file = pjoin(inputs_dir, "runs.hg19.bed")
+def test_close_to_hmer_run(resources_dir):
+    input_vcf = vcftools.get_vcf_df(pjoin(resources_dir, "hg19.vcf.gz"))
+    runs_file = pjoin(resources_dir, "runs.hg19.bed")
     result = close_to_hmer_run(input_vcf, runs_file)
     assert result["close_to_hmer_run"].sum() == 76
