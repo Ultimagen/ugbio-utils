602e9a4c17886f3f32755ce8acc00e9dc0e65f75
avigailmo <avigail.moldovan@ultimagen.com>
2025-02-05 09:12:34 +0200

    TestBufferedVariantReader

--- src/core/tests/unit/vcfbed/test_buffered_variants_reader.py
+++ src/core/tests/unit/vcfbed/test_buffered_variants_reader.py
@@ -1,4 +1,3 @@
-import unittest
 from pathlib import Path
 
 import pytest
@@ -10,16 +9,16 @@ def resources_dir():
     return Path(__file__).parent.parent.parent / "resources"
 
 
-class TestBufferedVariantReader(unittest.TestCase):
+class TestBufferedVariantReader:
     def test_get_variant(self, resources_dir):
         reader = BufferedVariantReader(f"{resources_dir}/single_sample_example.vcf.gz")
         variant_1 = reader.get_variant("chr1", 930196)
-        self.assertEqual(("T", "TG", "<NON_REF>"), variant_1.alleles)
+        assert ("T", "TG", "<NON_REF>") == variant_1.alleles
         variant_2 = reader.get_variant("chr1", 1044019)
-        self.assertEqual(("G", "GC", "<NON_REF>"), variant_2.alleles)
+        assert ("G", "GC", "<NON_REF>") == variant_2.alleles
         variant_3 = reader.get_variant("chr1", 10)
-        self.assertIsNone(variant_3)
+        assert variant_3
 
     def test_header(self, resources_dir):
         reader = BufferedVariantReader(f"{resources_dir}/single_sample_example.vcf.gz")
-        self.assertEqual("HG00239", reader.header.samples[0])
+        assert "HG00239" == reader.header.samples[0]
