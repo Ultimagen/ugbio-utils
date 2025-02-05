13567cb4723f4a2dece632e5777e312bad8d799d
avigailmo <avigail.moldovan@ultimagen.com>
2025-02-05 09:05:17 +0200

    move to unittest.mock.patch

--- src/filtering/tests/system/test_training_prep_system.py
+++ src/filtering/tests/system/test_training_prep_system.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from unittest.mock import patch
 
 import pandas as pd
 import pytest
@@ -10,16 +11,16 @@ def resources_dir():
     return Path(__file__).parent.parent / "resources"
 
 
-def test_prepare_ground_truth(mocker, tmpdir, resources_dir):
+@patch("ugbio_comparison.vcf_pipeline_utils.VcfPipelineUtils.run_vcfeval_concordance")
+@patch("ugbio_filtering.training_prep.calculate_labeled_vcf")
+def test_prepare_ground_truth(mock_run_vcfeval_concordance, mock_calculate_labeled_vcf, tmpdir, resources_dir):
     calls_file = str(Path(resources_dir, "input.vcf.gz"))
     vcfeval_output_file = Path(resources_dir, "vcfeval_output.vcf.gz")
     # bypass well-tested run_vcfeval_concordance
-    mocker.patch(
-        "ugbio_comparison.vcf_pipeline_utils.VcfPipelineUtils.run_vcfeval_concordance", return_value=vcfeval_output_file
-    )
+    mock_run_vcfeval_concordance.return_value = vcfeval_output_file
     joint_df = pd.read_hdf(str(Path(resources_dir, "labeled_df.h5")), key="df")
-    # bypass creating of the joint vcf (good unit test)
-    mocker.patch("ugbio_filtering.training_prep.calculate_labeled_vcf", return_value=joint_df)
+    # bypass creating of the joint vcf
+    mock_calculate_labeled_vcf.return_value = joint_df
     reference_path = str(Path(resources_dir, "ref_fragment.fa.gz"))
     output_file = str(Path(tmpdir, "output.h5"))
     tprep.prepare_ground_truth(calls_file, "", "", reference_path, output_file, chromosome=["chr21"])
