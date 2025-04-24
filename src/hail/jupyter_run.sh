2a996fb409f0e6986fed912d0ca6603594eba645
avigailmo <avigail.moldovan@ultimagen.com>
2025-04-24 13:32:49 +0300

    final fixes

--- src/hail/jupyter_run.sh
+++ src/hail/jupyter_run.sh
@@ -2,8 +2,8 @@
 
 export SPARK_HOME=/usr/lib/spark
 export PYSPARK_PYTHON=python3
-export HAIL_HOME=/opt/ugbio_utils/src/hail
-
+export HAIL_HOME=/opt/ugbio-utils/src/hail
+export PATH=$PATH:$HOME/.local/bin
 export PYTHONPATH="/home/hadoop/hail-python.zip:$SPARK_HOME/python:${SPARK_HOME}/python/lib/py4j-src.zip"
 echo "PYTHONPATH: ${PYTHONPATH}"
 
@@ -11,7 +11,7 @@ export PYSPARK_PYTHON=python3
 echo "PYSPARK_PYTHON: ${PYSPARK_PYTHON}"
 
 # Needed for HDFS
-JAR_PATH="/home/hadoop/hail-all-spark.jar:/usr/share/aws/emr/emrfs/lib/emrfs-hadoop-assembly-2.32.0.jar"
+JAR_PATH="/home/hadoop/hail-all-spark.jar:/usr/share/aws/emr/emrfs/lib/emrfs-hadoop-assembly-2.69.0.jar"
 export PYSPARK_SUBMIT_ARGS="--conf spark.driver.extraClassPath='$JAR_PATH' --conf spark.executor.extraClassPath='$JAR_PATH' pyspark-shell"
 echo "PYSPARK_SUBMIT_ARGS: ${PYSPARK_SUBMIT_ARGS}"
 
@@ -23,7 +23,7 @@ mkdir -p $HAIL_HOME/notebook/
 chmod -R 777 $HAIL_HOME/notebook
 cd $HAIL_HOME/notebook/
 
-JUPYTERPID=`cat /tmp/jupyter_notebook.pid` # Kill an existing Jupyter Lab if any running 
+JUPYTERPID=`cat /tmp/jupyter_notebook.pid` # Kill an existing Jupyter Lab if any running
 kill $JUPYTERPID
 nohup jupyter lab >/tmp/jupyter_notebook.log 2>&1 &
 echo $! > /tmp/jupyter_notebook.pid
