91df81f8a55773b75da7beb8c8c0b6772034a84b
GatKrieger <gatkrieger@ultimagen.com>
2025-07-23 09:59:49 +0000

    update test_genearte_mrd_report

--- src/mrd/tests/unit/test_generate_mrd_report.py
+++ src/mrd/tests/unit/test_generate_mrd_report.py
@@ -18,19 +18,30 @@ def output_path(tmpdir):
 def test_generate_mrd_report(output_path, resources_dir):
     mrd_report_inputs = MrdReportInputs(
         intersected_featuremaps_parquet=[
-            str(resources_dir / "416119_L7402.Pa_46_FreshFrozen.matched.intersection.parquet"),
-            str(resources_dir / "416119_L7402.Pa_67_FFPE.control.intersection.parquet"),
+            str(resources_dir / "Pa_46_333_LuNgs_08.Pa_46_FreshFrozen.matched.intersection.parquet"),
+            str(resources_dir / "Pa_46_333_LuNgs_08.Pa_67_FFPE.control.intersection.parquet"),
+            str(resources_dir / "Pa_46_333_LuNgs_08.syn0_Pa_46_FreshFrozen.db_control.intersection.parquet"),
+            str(resources_dir / "Pa_46_333_LuNgs_08.syn1_Pa_46_FreshFrozen.db_control.intersection.parquet"),
+            str(resources_dir / "Pa_46_333_LuNgs_08.syn2_Pa_46_FreshFrozen.db_control.intersection.parquet"),
+            str(resources_dir / "Pa_46_333_LuNgs_08.syn3_Pa_46_FreshFrozen.db_control.intersection.parquet"),
+            str(resources_dir / "Pa_46_333_LuNgs_08.syn4_Pa_46_FreshFrozen.db_control.intersection.parquet"),
         ],
-        matched_signatures_vcf_files=[str(resources_dir / "Pa_46_FreshFrozen.ann.chr20.filtered.vcf")],
-        control_signatures_vcf_files=[str(resources_dir / "Pa_67_FFPE.ann.chr20.filtered.vcf")],
-        # db_control_signatures_vcf_files=args_in.db_control_signatures_vcf,
-        coverage_bed=str(resources_dir / "416119_L7402.regions.bed.gz"),
+        matched_signatures_vcf_files=[str(resources_dir / "Pa_46_FreshFrozen.ann.chr20.filtered.vcf.gz")],
+        control_signatures_vcf_files=[str(resources_dir / "Pa_67_FFPE.ann.chr20.filtered.vcf.gz")],
+        db_control_signatures_vcf_files=[
+            str(resources_dir / "syn0_Pa_46_FreshFrozen.ann.chr20.filtered_pancan.filtered.filtered.vcf.gz"),
+            str(resources_dir / "syn1_Pa_46_FreshFrozen.ann.chr20.filtered_pancan.filtered.filtered.vcf.gz"),
+            str(resources_dir / "syn2_Pa_46_FreshFrozen.ann.chr20.filtered_pancan.filtered.filtered.vcf.gz"),
+            str(resources_dir / "syn3_Pa_46_FreshFrozen.ann.chr20.filtered_pancan.filtered.filtered.vcf.gz"),
+            str(resources_dir / "syn4_Pa_46_FreshFrozen.ann.chr20.filtered_pancan.filtered.filtered.vcf.gz"),
+        ],
+        coverage_bed=str(resources_dir / "Pa_46_333_LuNgs_08.regions.bed.gz"),
         # tumor_sample=args_in.tumor_sample,
         output_dir=output_path,
         output_basename="test_report",
-        featuremap_file=str(resources_dir / "416119_L7402.featuremap_df.parquet"),
+        featuremap_file=str(resources_dir / "Pa_46_333_LuNgs_08.featuremap_df.parquet"),
         signature_filter_query="(norm_coverage <= 2.5) and (norm_coverage >= 0.6)",
-        read_filter_query="edist < 5",  # only for testing this non-matching files, otherwise take "qual > 60"
+        read_filter_query="snvq>40",  # only for testing this non-matching files, otherwise take "qual > 60"
     )
 
     output_report_html = generate_mrd_report(mrd_report_inputs)
@@ -43,7 +54,7 @@ def test_generate_mrd_report(output_path, resources_dir):
 
     # test h5 output
     h5_output = str(output_path / "test_report.tumor_fraction.h5")
-    h5_expected = str(resources_dir / "416119_L7402.tumor_fraction.h5")
+    h5_expected = str(resources_dir / "test_report.tumor_fraction.expected_output.h5")
     with pd.HDFStore(h5_expected) as store:
         h5_keys = store.keys()
     with pd.HDFStore(h5_output) as store:
