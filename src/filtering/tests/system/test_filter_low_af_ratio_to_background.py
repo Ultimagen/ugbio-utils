768dd6b05f4cbe3071e3392a4587136e840ba5dc
Ilya Soifer <ilya.soifer@ultimagen.com>
2025-08-10 09:06:06 +0300

    [BIOIN-2300] Improve AFRatioFiltering script for hmer indels (#148)
    * added allowance for minimal VAF for hmer indels
    * Added a parameter to disable AF ratio filtering in h-indels if tumor frequency is high
    * change default values of h-indel AF ratio filtering

--- src/filtering/tests/system/test_filter_low_af_ratio_to_background.py
+++ src/filtering/tests/system/test_filter_low_af_ratio_to_background.py
@@ -1,10 +1,12 @@
+from pathlib import Path
+
 import pysam
 import pytest
 from ugbio_filtering.filter_low_af_ratio_to_background import filter_low_af_ratio_to_background
 
 
 @pytest.fixture
-def example_vcf(tmp_path):
+def example_vcf(tmp_path: Path):
     vcf_content = """##fileformat=VCFv4.2
 ##FILTER=<ID=RefCall,Description="Reference call">
 ##INFO=<ID=VARIANT_TYPE,Number=1,Type=String,Description="Type of variant">
@@ -13,14 +15,20 @@ def example_vcf(tmp_path):
 ##FORMAT=<ID=DP,Number=1,Type=Integer,Description="Read depth">
 ##FORMAT=<ID=BG_AD,Number=R,Type=Integer,Description="Background allelic depths">
 ##FORMAT=<ID=BG_DP,Number=1,Type=Integer,Description="Background read depth">
+##FORMAT=<ID=VAF,Number=A,Type=Float,Description="Variant allele fractions">
+##FORMAT=<ID=BG_VAF,Number=A,Type=Float,Description="Background variant allele fractions">
 ##contig=<ID=chr1,length=248956422>
 #CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\tsample1
-chr1\t100\t.\tA\tT\t.\tPASS\tVARIANT_TYPE=snp\tGT:AD:DP:BG_AD:BG_DP\t0/1:5,10:15:1,1:2
-chr1\t200\t.\tG\tC\t.\tPASS\tVARIANT_TYPE=snp\tGT:AD:DP:BG_AD:BG_DP\t0/1:2,2:4:1,1:2
-chr1\t300\t.\tT\tG\t.\tRefCall\tVARIANT_TYPE=snp\tGT:AD:DP:BG_AD:BG_DP\t0/1:10,10:20:1,1:2
-chr1\t400\t.\tC\tA\t.\tPASS\tVARIANT_TYPE=h-indel\tGT:AD:DP:BG_AD:BG_DP\t0/1:10,10:20:1,1:2
-chr1\t400\t.\tC\tA,T\t.\tPASS\tVARIANT_TYPE=non-h-indel\tGT:AD:DP:BG_AD:BG_DP\t0/2:10,30,10:50:10,10,1:21
-chr1\t400\t.\tC\tA,T\t.\tPASS\tVARIANT_TYPE=non-h-indel\tGT:AD:DP:BG_AD:BG_DP\t0/2:10,30,100:140:10,10,1:21
+chr1\t100\t.\tA\tT\t.\tPASS\tVARIANT_TYPE=snp\tGT:AD:DP:BG_AD:BG_DP:VAF:BG_VAF\t0/1:5,10:15:1,1:2:0.6667:0.5
+chr1\t200\t.\tG\tC\t.\tPASS\tVARIANT_TYPE=snp\tGT:AD:DP:BG_AD:BG_DP:VAF:BG_VAF\t0/1:2,2:4:1,1:2:0.5:0.5
+chr1\t300\t.\tT\tG\t.\tRefCall\tVARIANT_TYPE=snp\tGT:AD:DP:BG_AD:BG_DP:VAF:BG_VAF\t0/1:10,10:20:1,1:2:0.5:0.5
+chr1\t400\t.\tCTA\tC\t.\tPASS\tVARIANT_TYPE=non-h-indel\tGT:AD:DP:BG_AD:BG_DP:VAF:BG_VAF\t0/1:10,10:20:2,1:3:0.5:0.33333
+chr1\t500\t.\tC\tCA\t.\tPASS\tVARIANT_TYPE=h-indel\tGT:AD:DP:BG_AD:BG_DP:VAF:BG_VAF\t0/1:10,90:100:9,1:10:0.9:0.1
+chr1\t600\t.\tC\tCTA,T\t.\tPASS\tVARIANT_TYPE=non-h-indel\tGT:AD:DP:BG_AD:BG_DP:VAF:BG_VAF\t0/2:10,30,10:50:10,10,1:21:0.6,0.2:0.47619,0.04762
+chr1\t700\t.\tC\tCTA,T\t.\tPASS\tVARIANT_TYPE=non-h-indel\tGT:AD:DP:BG_AD:BG_DP:VAF:BG_VAF\t0/2:10,30,100:140:10,10,1:21:0.21428,0.71428:0.47619,0.04762
+chr1\t700\t.\tC\tCTA,T\t.\tPASS\tVARIANT_TYPE=non-h-indel\tGT:AD:DP:BG_AD:BG_DP:VAF:BG_VAF\t0/1:10,30,100:140:10,10,1:21:0.21428,0.71428:0.47619,0.04762
+chr1\t800\t.\tC\tCA,T\t.\tPASS\tVARIANT_TYPE=h-indel\tGT:AD:DP:BG_AD:BG_DP:VAF:BG_VAF\t0/1:10,30,100:140:10,30,100:140:0.21428,0.71428:0.21428,0.71428
+chr1\t900\t.\tC\tCA\t.\tPASS\tVARIANT_TYPE=h-indel\tGT:AD:DP:BG_AD:BG_DP:VAF:BG_VAF\t0/1:1,10:11:1,10:11:0.0909:0.90909
 """
     vcf_path = tmp_path / "input.vcf.gz"
     with open(vcf_path, "w") as f:
@@ -28,43 +36,76 @@ chr1\t400\t.\tC\tA,T\t.\tPASS\tVARIANT_TYPE=non-h-indel\tGT:AD:DP:BG_AD:BG_DP\t0
     return str(vcf_path)
 
 
-def test_filter_low_af_ratio_to_background_basic(example_vcf, tmp_path):
+def test_filter_low_af_ratio_to_background_basic(example_vcf: str, tmp_path: Path):
     output_vcf = tmp_path / "output.vcf.gz"
     filter_low_af_ratio_to_background(
-        input_vcf=example_vcf, output_vcf=str(output_vcf), af_ratio_threshold=10, new_filter="LowAFRatioToBackground"
+        input_vcf=example_vcf,
+        output_vcf=str(output_vcf),
+        af_ratio_threshold=10,
+        af_ratio_threshold_h_indels=2,
+        t_vaf_threshold=0.15,
+        new_filter="LowAFRatioToBackground",
     )
     with pysam.VariantFile(str(output_vcf)) as vcf:
         records = list(vcf.fetch())
-        # First record: AF ratio = (10/15)/(1/2) = (0.6667)/(0.5) = 1.333 < 10, should be filtered
+        # First record: VARIANT_TYPE=snp, AF ratio = 0.6667/0.5 = 1.333 < 10,
+        # should be filtered
         assert "LowAFRatioToBackground" in records[0].filter.keys()
-        # Second record: AF ratio = (2/4)/(1/2) = 0.5/0.5 = 1 < 10, should be filtered
+        # Second record: VARIANT_TYPE=snp, AF ratio = 0.5/0.5 = 1 < 10,
+        # should be filtered
         assert "LowAFRatioToBackground" in records[1].filter.keys()
-        # Third record: FILTER=RefCall, should not be filtered
+        # Third record: VARIANT_TYPE=snp, FILTER=RefCall,
+        # should not be filtered
         assert "LowAFRatioToBackground" not in records[2].filter.keys()
         assert "RefCall" in records[2].filter.keys()
-        # Fourth record: VARIANT_TYPE=h-indel, should not be filtered
-        assert "LowAFRatioToBackground" not in records[3].filter.keys()
-        # Fifth record: AF ratio in alt allele 2 = (10/50)/(1/21) = 4.2 < 10, should be filtered
-        assert "LowAFRatioToBackground" in records[4].filter.keys()
-        # Sixth record: AF ratio in alt allele 2 = (100/140)/(1/21) = 15 > 10, should not be filtered
-        assert "LowAFRatioToBackground" not in records[5].filter.keys()
+        # Fourth record: VARIANT_TYPE=non-h-indel, AF ratio = 0.5/0.33333 = 1.5 < 10,
+        # should be filtered
+        assert "LowAFRatioToBackground" in records[3].filter.keys()
+        # Fifth record: VARIANT_TYPE=h-indel, AF ratio = 0.9/0.1 = 9 > 2, VAF > 0.15,
+        # should not be filtered
+        assert "LowAFRatioToBackground" not in records[4].filter.keys()
+        # Sixth record: VARIANT_TYPE=non-h-indel, AF ratio in alt allele 2 = 0.2/0.04762 = 4.2 < 10,
+        # should be filtered
+        assert "LowAFRatioToBackground" in records[5].filter.keys()
+        # Seventh record: VARIANT_TYPE=non-h-indel, AF ratio in alt allele 2 = 0.71428/0.04762 = 15 > 10,
+        # should not be filtered
+        assert "LowAFRatioToBackground" not in records[6].filter.keys()
+        # Eighth record: VARIANT_TYPE=non-h-indel, AF ratio in alt allele 1 = 0.21428/0.47619 = 0.45 < 10,
+        # should be filtered
+        assert "LowAFRatioToBackground" in records[7].filter.keys()
+        # Ninth record: VARIANT_TYPE=h-indel, AF ratio in alt allele 1 = 0.21428/0.21428 = 1 < 2, VAF > 0.15,
+        # should not be filtered
+        assert "LowAFRatioToBackground" not in records[8].filter.keys()
+        # Tenth record: VARIANT_TYPE=h-indel, AF ratio = 0.0909/0.90909 = 0.1 < 2, VAF < 0.15,
+        # should be filtered
+        assert "LowAFRatioToBackground" in records[9].filter.keys()
 
 
-def test_filter_low_af_ratio_to_background_no_fail(example_vcf, tmp_path):
+def test_filter_low_af_ratio_to_background_no_fail(example_vcf: str, tmp_path: Path):
     output_vcf = tmp_path / "output2.vcf.gz"
     # Use a low threshold so nothing is filtered
     filter_low_af_ratio_to_background(
-        input_vcf=example_vcf, output_vcf=str(output_vcf), af_ratio_threshold=0.05, new_filter="LowAFRatioToBackground"
+        input_vcf=example_vcf,
+        output_vcf=str(output_vcf),
+        af_ratio_threshold=0.05,
+        af_ratio_threshold_h_indels=0.05,
+        t_vaf_threshold=1,
+        new_filter="LowAFRatioToBackground",
     )
     with pysam.VariantFile(str(output_vcf)) as vcf:
         for rec in vcf.fetch():
             assert "LowAFRatioToBackground" not in rec.filter.keys()
 
 
-def test_filter_low_af_ratio_to_background_custom_filter(example_vcf, tmp_path):
+def test_filter_low_af_ratio_to_background_custom_filter(example_vcf: str, tmp_path: Path):
     output_vcf = tmp_path / "output3.vcf.gz"
     filter_low_af_ratio_to_background(
-        input_vcf=example_vcf, output_vcf=str(output_vcf), af_ratio_threshold=10, new_filter="MyCustomFilter"
+        input_vcf=example_vcf,
+        output_vcf=str(output_vcf),
+        af_ratio_threshold=10,
+        af_ratio_threshold_h_indels=2,
+        t_vaf_threshold=0.5,
+        new_filter="MyCustomFilter",
     )
     with pysam.VariantFile(str(output_vcf)) as vcf:
         records = list(vcf.fetch())
--- src/filtering/ugbio_filtering/filter_low_af_ratio_to_background.py
+++ src/filtering/ugbio_filtering/filter_low_af_ratio_to_background.py
@@ -1,74 +1,114 @@
 import argparse
 
 import pysam
+import tqdm.auto as tqdm
+from ugbio_core.logger import logger
 
 
 def filter_low_af_ratio_to_background(
-    input_vcf, output_vcf, af_ratio_threshold=10, new_filter="LowAFRatioToBackground"
+    input_vcf: str,
+    output_vcf: str,
+    af_ratio_threshold: float = 10,
+    af_ratio_threshold_h_indels: float = 0,
+    t_vaf_threshold: float = 0,
+    new_filter: str = "LowAFRatioToBackground",
 ):
     vcf_in = pysam.VariantFile(input_vcf)
 
-    # Add a new FILTER definition to the header
-    if new_filter not in vcf_in.header.filters:
-        filter_desc = f"For snps and non-h-indels: \
-            filter if AF ratio to background in GT ALT alleles < {af_ratio_threshold}"
+    # Check if the new filter already exists in the header
+    if new_filter in vcf_in.header.filters:
+        logger.warning(f"Existing {new_filter} filter found in header. It will be replaced.")
+        # NOTE: if there is an existing filter with the same name, the description will NOT be updated,
+        # but the records will be updated with the new filter logic
+    else:
+        # Add a new FILTER definition to the header
+        filter_desc = (
+            f"Filter variants if AF ratio to background in GT ALT alleles < threshold. "
+            f"For snps and non-h-indels: {af_ratio_threshold}, and "
+            f"for h-indels (applied only if tumor VAF<{t_vaf_threshold}: {af_ratio_threshold_h_indels})"
+        )
         vcf_in.header.filters.add(new_filter, None, None, filter_desc)
 
     vcf_out = pysam.VariantFile(output_vcf, "w", header=vcf_in.header)
-
-    for record in vcf_in.fetch():
-        # Skip if variant is marked RefCall or if it is an h-indel
-        if (record.filter.keys() == ["RefCall"]) | (record.info.get("VARIANT_TYPE") == "h-indel"):
+    logger.info(f"Processing {input_vcf} and writing to {output_vcf}")
+    filtered_count = 0
+    for record in tqdm.tqdm(vcf_in.fetch()):
+        # Skip if variant is marked RefCall
+        if "RefCall" in list(record.filter.keys()):
             vcf_out.write(record)
             continue
         else:
-            failed = process_record(record, af_ratio_threshold)
+            # Remove the AF ratio filter if it exists in the record
+            if new_filter in record.filter.keys():
+                # Get current filters
+                current_filters = [str(x) for x in list(record.filter.keys())]
+                # Reassign without the AF ratio filter
+                record.filter.clear()
+                for f in current_filters:
+                    if f != new_filter:
+                        record.filter.add(f)
+
+            if len(record.filter) == 0:
+                record.filter.clear()  # ensures FILTER=PASS in output
+
+            threshold_to_use = (
+                af_ratio_threshold_h_indels if record.info.get("VARIANT_TYPE") == "h-indel" else af_ratio_threshold
+            )
+            vaf_threshold_to_use = t_vaf_threshold if record.info.get("VARIANT_TYPE") == "h-indel" else 1
+
+            failed = process_record(record, threshold_to_use, vaf_threshold_to_use)
+
             if failed:
                 record.filter.add(new_filter)
+                filtered_count += 1
 
         vcf_out.write(record)
-
+    logger.info(f"Filtered {filtered_count} variants with {new_filter} filter.")
     vcf_in.close()
     vcf_out.close()
 
     pysam.tabix_index(output_vcf, preset="vcf", force=True)
 
-    print(f"Annotated VCF written to: {output_vcf}")
+    logger.info(f"Annotated VCF written to: {output_vcf}")
 
 
-def process_record(record, af_ratio_threshold):
+def process_record(record, af_ratio_threshold, t_vaf_threshold):
     failed = False
     for sample in record.samples:
         gt = record.samples[sample].get("GT")
-        ad = record.samples[sample].get("AD")
-        dp = record.samples[sample].get("DP")
-        bg_ad = record.samples[sample].get("BG_AD")
-        bg_dp = record.samples[sample].get("BG_DP")
-
-        if gt is None or ad is None:
+        vaf = record.samples[sample].get("VAF")
+        bg_vaf = record.samples[sample].get("BG_VAF")
+        t_vaf = record.samples[sample].get("VAF")
+        if gt is None or vaf is None or bg_vaf is None:
             continue
 
         for allele in gt:
-            if (allele is None) or (allele == 0) or (dp == 0) or (bg_dp == 0):
+            if (allele is None) or (allele == 0):
                 continue  # skip REF or missing
-            elif ad[allele] == 0:
+            elif (allele - 1 >= len(vaf)) or (allele - 1 >= len(bg_vaf)):
+                continue  # skip if allele index is out of bounds
+            elif vaf[allele - 1] is None or vaf[allele - 1] == 0:
                 # allele is not present in the sample, so filter this allele
                 failed = True
                 continue
-            elif bg_ad[allele] == 0:
+            elif bg_vaf[allele - 1] is None or bg_vaf[allele - 1] == 0:
                 # the allele is present in the sample but not in the background,
                 # so do not filter the variant
                 failed = False
                 break
-            elif bg_ad[allele] > 0:
-                af_ratio = (ad[allele] / dp) / (bg_ad[allele] / bg_dp)
-                if af_ratio >= af_ratio_threshold:
+            else:  # bg_vaf[allele - 1] > 0
+                af_ratio = vaf[allele - 1] / bg_vaf[allele - 1]
+                if t_vaf is None or t_vaf[allele - 1] is None:
+                    logger.warning("Tumor VAF is None for a GT allele!")
+                elif (af_ratio >= af_ratio_threshold) or (t_vaf[allele - 1] >= t_vaf_threshold):
                     # there is an allele with AF ratio >= threshold,
+                    # or there is an allele where t_vaf is above threshold
                     # so do not filter the variant
                     failed = False
                     break
                 else:
-                    # this allele has AF ratio < threshold, so filter this allele
+                    # this allele has AF ratio < threshold (and tumor vaf < vaf_threshold for h-indels),
+                    # so filter this allele
                     failed = True
 
     return failed
@@ -78,7 +118,23 @@ def main():
     parser = argparse.ArgumentParser(description="Annotate variants with low AF ratio")
     parser.add_argument("input_vcf", help="Input VCF file")
     parser.add_argument("output_vcf", help="Output VCF file")
-    parser.add_argument("--af_ratio_threshold", type=float, default=10, help="AF ratio threshold (default: 10)")
+    parser.add_argument(
+        "--af_ratio_threshold",
+        type=float,
+        default=10,
+        help="AF ratio threshold for snps and non-h-indels (default: 10)",
+    )
+    parser.add_argument(
+        "--tumor_vaf_threshold_h_indels",
+        type=float,
+        default=0,
+        help="Tumor VAF threshold for filtering (default: 0) - \
+            any h-indel with VAF above this threshold will not be filtered",
+    )
+    parser.add_argument(
+        "--af_ratio_threshold_h_indels", type=float, default=0, help="AF ratio threshold for h-indels (default: 0)"
+    )
+
     parser.add_argument(
         "--new_filter",
         default="LowAFRatioToBackground",
@@ -91,6 +147,8 @@ def main():
         input_vcf=args.input_vcf,
         output_vcf=args.output_vcf,
         af_ratio_threshold=args.af_ratio_threshold,
+        af_ratio_threshold_h_indels=args.af_ratio_threshold_h_indels,
+        t_vaf_threshold=args.tumor_vaf_threshold_h_indels,
         new_filter=args.new_filter,
     )
 
