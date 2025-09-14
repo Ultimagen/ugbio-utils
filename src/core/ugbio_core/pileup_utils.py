3e5842a9302c0a9f6c4d2aac5418f41889fbc8a3
TammushB <tammy.biniashvili@ultimagen.com>
2025-09-14 14:10:11 +0000

    move pileuptofreq to pileup_utils

--- src/core/ugbio_core/pileup_utils.py
+++ src/core/ugbio_core/pileup_utils.py
@@ -1,3 +1,86 @@
+from collections import defaultdict
+
+import pandas as pd
+
+PILEUP_IGNORE = {",", ".", "*", "#", ">", "<"}
+USE_STARTEND = True
+START_MARKER_LENGTH = 2
+
+
+def pileup_to_freq(reference: str, pileup: str) -> dict:  # noqa: C901
+    """Counts A, C, G, T, In, Del occurrence given a samtools pileup string"""
+    pileup = pileup.upper()
+    frequencies = defaultdict(int)
+    is_indel = False
+    indel_pos = 0
+    indel_size = ""
+    is_start = 0
+    last_base = ""
+    for k in pileup:
+        if k in ("+", "-"):
+            is_indel = True
+            indel_pos = 0
+            indel_size = ""
+            indel = k
+            continue
+        if k == "$":
+            frequencies[last_base] -= not USE_STARTEND
+            continue
+        if k == "^":
+            is_start = 1
+        if is_start > 0:
+            if is_start < START_MARKER_LENGTH:
+                is_start += 1
+            elif is_start == START_MARKER_LENGTH:
+                frequencies[k] += USE_STARTEND
+                is_start = 0
+            continue
+            continue
+        if is_indel:
+            is_digit = str.isdigit(k)
+            if is_digit:
+                indel_size += k
+                continue
+            indel_pos += 1
+            indel += k
+            if indel_pos == int(indel_size):
+                frequencies[indel] += 1
+                is_indel = False
+                indel_pos = 0
+            continue
+        last_base = k
+        frequencies[k] += 1
+    frequencies[reference] += frequencies[","] + frequencies["."]
+    frequencies["Deletion"] = frequencies["*"] + frequencies["#"]
+    return {k: v for k, v in frequencies.items() if v > 0 and k not in PILEUP_IGNORE}
+
+
+def create_frequencies_from_pileup(input_pileup_file) -> pd.DataFrame:
+    """Create a pandas DataFrame summarizing samtools pileup file results"""
+    df_frequencies = pd.DataFrame(columns=["Depth", "Chrom", "Pos", "Ref", "Sample", "Base", "Count"])
+    data = []
+    import fileinput
+
+    with fileinput.input(input_pileup_file, mode="r") as fin:
+        for line in fin:
+            sample = 0
+            values = line.split("\t")
+            chr_name = values[0]
+            pos = values[1]
+            ref = values[2]
+            dp = values[3]
+            for i in range(4, len(values), 3):
+                sample += 1
+                if values[i - 1] == "0":
+                    continue
+                freq = pileup_to_freq(values[2], values[i])
+                for base, count in freq.items():
+                    data.append([dp, chr_name, pos, ref, sample, base, count])
+    fin.close()
+    df_frequencies = pd.DataFrame(data, columns=["Depth", "Chrom", "Pos", "Ref", "Sample", "Base", "Count"])
+    return df_frequencies
+
+
 def parse_bases(bases: str) -> tuple[int, int]:
     """
     Parse base calls from mpileup format and count reference and non-reference bases.
