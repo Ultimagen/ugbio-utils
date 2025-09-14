cb5e850bb9f15b7eb20760e970b9a4b612c183fb
TammushB <tammy.biniashvili@ultimagen.com>
2025-09-14 10:12:19 +0000

    remove comments

--- src/core/ugbio_core/vcfbed/vcftools.py
+++ src/core/ugbio_core/vcfbed/vcftools.py
@@ -12,9 +12,6 @@ import numpy as np
 import pandas as pd
 import pysam
 
-# def index_vcf(sp, vcf_file):
-#     sp.print_and_run(f"bcftools index -t {vcf_file}")
-
 
 def get_vcf_df(  # noqa: PLR0912, C901
     variant_calls: str | pysam.VariantFile,
