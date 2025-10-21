9e5892215acbd0dc3358393bf1065ece4b55f5ea
TammushB <tammy.biniashvili@ultimagen.com>
2025-10-21 10:56:28 +0000

    split integrate_tandem_repeat_features to helper functions

--- src/featuremap/ugbio_featuremap/somatic_featuremap_utils.py
+++ src/featuremap/ugbio_featuremap/somatic_featuremap_utils.py
@@ -59,6 +59,79 @@ def _prepare_annotation_files(tmpdir, tr_tsv_file):
     return gz_tsv, hdr_file
 
 
+def _create_variant_bed(merged_vcf, bed_file):
+    """Create a BED file from VCF variants."""
+    cmd_bcftools = ["bcftools", "query", "-f", "%CHROM\t%POS\t%END\n", merged_vcf]
+    cmd_awk = ["awk", 'BEGIN{OFS="\t"} {print $1, $2, $3+1}']
+    with open(bed_file, "w") as out_file:
+        p1 = subprocess.Popen(cmd_bcftools, stdout=subprocess.PIPE)
+        p2 = subprocess.Popen(cmd_awk, stdin=p1.stdout, stdout=out_file)
+        p1.stdout.close()
+        p2.communicate()
+        p1.wait()
+
+
+def _find_closest_tandem_repeats(bed1, bed2, output_file):
+    """Find closest tandem repeats for each variant."""
+    cmd_bedtools = ["bedtools", "closest", "-D", "ref", "-a", bed1, "-b", bed2]
+    cmd_cut = ["cut", "-f1-2,5-8"]
+    with open(output_file, "w") as out_file:
+        p1 = subprocess.Popen(cmd_bedtools, stdout=subprocess.PIPE)
+        p2 = subprocess.Popen(cmd_cut, stdin=p1.stdout, stdout=out_file)
+        p1.stdout.close()
+        p2.communicate()
+        p1.wait()
+
+
+def _process_tandem_repeat_data(tr_tsv_file):
+    """Process tandem repeat data and calculate metrics."""
+    df_tr_info = pd.read_csv(tr_tsv_file, sep="\t", header=None)
+    df_tr_info.columns = ["chrom", "pos", "TR_START", "TR_END", "TR_SEQ", "TR_DISTANCE"]
+    df_tr_info["TR_LENGTH"] = df_tr_info["TR_END"] - df_tr_info["TR_START"]
+
+    extracted = df_tr_info["TR_SEQ"].str.extract(r"\((\w+)\)")
+    df_tr_info["TR_SEQ_UNIT_LENGTH"] = extracted[0].str.len()
+
+    if df_tr_info["TR_SEQ_UNIT_LENGTH"].isna().any():
+        logger.warning(
+            "Some TR_SEQ values did not match the expected pattern '(unit)'. "
+            "Setting TR_SEQ_UNIT_LENGTH to 0 for these rows."
+        )
+        df_tr_info["TR_SEQ_UNIT_LENGTH"] = df_tr_info["TR_SEQ_UNIT_LENGTH"].fillna(0).astype(int)
+
+    df_tr_info.to_csv(tr_tsv_file, sep="\t", header=None, index=False)
+
+
+def _prepare_annotation_files(tmpdir, tr_tsv_file):
+    """Prepare files for VCF annotation."""
+    sorted_tsv = pjoin(tmpdir, "merged_vcf.tmp.TRdata.sorted.tsv")
+    cmd = ["sort", "-k1,1", "-k2,2n", tr_tsv_file]
+    with open(sorted_tsv, "w") as out_file:
+        subprocess.check_call(cmd, stdout=out_file)
+
+    gz_tsv = sorted_tsv + ".gz"
+    cmd = ["bgzip", "-c", sorted_tsv]
+    with open(gz_tsv, "wb") as out_file:
+        subprocess.check_call(cmd, stdout=out_file)
+
+    cmd = ["tabix", "-s", "1", "-b", "2", "-e", "2", gz_tsv]
+    subprocess.check_call(cmd)
+
+    hdr_txt = [
+        '##INFO=<ID=TR_START,Number=1,Type=String,Description="Closest tandem Repeat Start">',
+        '##INFO=<ID=TR_END,Number=1,Type=String,Description="Closest Tandem Repeat End">',
+        '##INFO=<ID=TR_SEQ,Number=1,Type=String,Description="Closest Tandem Repeat Sequence">',
+        '##INFO=<ID=TR_DISTANCE,Number=1,Type=String,Description="Closest Tandem Repeat Distance">',
+        '##INFO=<ID=TR_LENGTH,Number=1,Type=String,Description="Closest Tandem Repeat total length">',
+        '##INFO=<ID=TR_SEQ_UNIT_LENGTH,Number=1,Type=String,Description="Closest Tandem Repeat unit length">',
+    ]
+    hdr_file = pjoin(tmpdir, "tr_hdr.txt")
+    with open(hdr_file, "w") as f:
+        f.writelines(line + "\n" for line in hdr_txt)
+
+    return gz_tsv, hdr_file
+
+
 def integrate_tandem_repeat_features(merged_vcf, ref_tr_file, out_dir):
     """
     Integrate tandem repeat features into a VCF file.
