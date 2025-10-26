84555d640ce5f8f837bc4305ebd9fcd608c5dc79
avigailmo <avigail.moldovan@ultimagen.com>
2025-10-26 09:02:52 +0200

    report_missing_runs script

--- src/omics/ugbio_omics/report_missing_runs.py
+++ src/omics/ugbio_omics/report_missing_runs.py
@@ -1,25 +1,27 @@
 import json
 import logging
+import time
 from argparse import ArgumentParser
 
 import boto3
+from report_run_completion import report_run_completion
 
 logging.basicConfig(level=logging.INFO)
 
-LAMBDA_FUNCTION_NAME = "omicsEndRunHandler"
+LAMBDA_FUNCTION_NAME = "omicsWorkflowStartHandler"
 SUCCESS_STATUS_CODE = 200
 
 
-def report_run_completion(run_ids: list, aws_region):
+def report_run_start(run_ids: list, aws_region):
     account_id = boto3.client("sts").get_caller_identity().get("Account")
     lambda_client = boto3.client("lambda", region_name=aws_region)
     for run_id in run_ids:
         lambda_input = {
             "detail-type": "Run Status Change (Manual)",
-            "source": "ugbio_omics.report_run_completion",
+            "source": "ugbio_omics.report_run_start",
             "account": account_id,
             "region": aws_region,
-            "runAnalyzerResult": {"Payload": {"run_id": run_id}},
+            "resources": [f"arn:aws:omics:{aws_region}:{account_id}:run/{run_id}"],
         }
         response = lambda_client.invoke(
             FunctionName=LAMBDA_FUNCTION_NAME, InvocationType="RequestResponse", Payload=json.dumps(lambda_input)
@@ -31,10 +33,12 @@ def report_run_completion(run_ids: list, aws_region):
 
 
 def main(raw_args=None):
-    parser = ArgumentParser(description=f"Reports completion of Omics Run by invoking {LAMBDA_FUNCTION_NAME} lambda")
+    parser = ArgumentParser(description="Reports start and End of Omics Runs that are missing from the database")
     parser.add_argument("run_ids", help="Omics run ids (space separated)", nargs="+")
     parser.add_argument("-r", "--aws_region", type=str, help="aws region", default="us-east-1")
     args = parser.parse_args(raw_args)
+    report_run_start(args.run_ids, args.aws_region)
+    time.sleep(30)  # sleep for 30 seconds to allow the start event to be processed
     report_run_completion(args.run_ids, args.aws_region)
 
 
