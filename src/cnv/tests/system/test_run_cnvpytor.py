e72d0a3afa159e6c6d6f9412557134352a012ab6
Ilya Soifer <ilya.soifer@ultimagen.com>
2026-01-11 18:45:26 +0000

    Fix numeric instability

--- src/cnv/tests/system/test_run_cnvpytor.py
+++ src/cnv/tests/system/test_run_cnvpytor.py
@@ -1,7 +1,7 @@
-import filecmp
 import os
 from pathlib import Path
 
+import pandas as pd
 import pytest
 from ugbio_cnv import run_cnvpytor
 
@@ -39,4 +39,9 @@ class TestRunCNVpytor:
 
         out_cnvs_file = os.path.join(tmp_path, f"{sample_name}.pytor.bin{bin_size}.CNVs.tsv")
         expected_out_cnvs_file = os.path.join(resources_dir, "HG002.pytor.bin500.CNVs.tsv")
-        assert filecmp.cmp(out_cnvs_file, expected_out_cnvs_file)
+
+        # Read both files into dataframes and compare with tolerance for floats
+        df_actual = pd.read_csv(out_cnvs_file, sep="\t")
+        df_expected = pd.read_csv(expected_out_cnvs_file, sep="\t")
+
+        pd.testing.assert_frame_equal(df_actual, df_expected, rtol=1e-5, atol=1e-8)
