7483b5d5e48028ff050bf7a60f4acaf1dbd42a71
GatKrieger <gatkrieger@ultimagen.com>
2026-03-02 09:32:26 +0000

    after Itai's comment

--- src/srsnv/ugbio_srsnv/srsnv_report.py
+++ src/srsnv/ugbio_srsnv/srsnv_report.py
@@ -108,7 +108,7 @@ def compute_is_cycle_skip_column(data_df: pd.DataFrame, flow_order: str = "TGCA"
         how="left",
     )[IS_CYCLE_SKIP]
 
-    result.index = data_df.index
+    result = result.loc[data_df.index]
 
     return result
 
